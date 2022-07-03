0857961cb9a63c2cef8e3f396432ee9c8b254ea4
Bobby Lee <superbahbi@gmail.com>
2022-07-03 12:36:28 -0700

    added env constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
+export const __db_type__ = process.env.DB_TYPE;
+export const __db_name__ = process.env.DB_NAME;
+export const __db_user__ = process.env.DB_USER;
+export const __db_pass__ = process.env.DB_PASS;
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,12 @@
 require("dotenv").config();
 import { Post } from "./entities/Post";
-import { __prod__ } from "./constants";
+import {
+  __prod__,
+  __db_type__,
+  __db_name__,
+  __db_user__,
+  __db_pass__,
+} from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 
@@ -10,10 +16,10 @@ export default {
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [Post],
-  type: process.env.DB_TYPE,
-  dbName: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  type: __db_type__,
+  dbName: __db_name__,
+  user: __db_user__,
+  password: __db_pass__,
   debug: !__prod__,
   allowGlobalContext: true,
 } as Parameters<typeof MikroORM.init>[0];
