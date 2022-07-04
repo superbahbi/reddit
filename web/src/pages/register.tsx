a43314bf5cb55fb9487ae4a2217e6d4bcd654162
Bobby Lee <superbahbi@gmail.com>
2022-07-03 20:03:26 -0700

    added web server files

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,47 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+export default Register;
