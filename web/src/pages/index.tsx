a2ef9c28444504c4aa3aeb7552e38e3304a50adf
Bobby Lee <superbahbi@gmail.com>
2022-07-04 17:31:15 -0700

    added post data  using urql in index

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,21 @@
 import { NavBar } from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>Loading..</div>
+      ) : (
+        data.posts.map((post) => <div>{post.title}</div>)
+      )}
+    </>
+  );
+};
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
-
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
